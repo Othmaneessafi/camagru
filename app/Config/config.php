606c7af328dabefdae6759c5f067ea744a493f4d
OthmaneOthy <44867969+OthmaneOthy@users.noreply.github.com>
2021-01-17 15:15:54 +0100

    ok

--- app/Config/config.php
+++ app/Config/config.php
@@ -1,10 +1,10 @@
 <?php
 
-    define('DB_HOST', '192.168.99.112:2021');
+    define('DB_HOST', '192.168.99.113:2021');
     define('DB_USER', 'root');
     define('DB_PWD', 'myrootpass');
     define('DB_NAME', 'camagru_db');
 
     define('CAMAGRU_ROOT', dirname(dirname(__FILE__)));
-    define('URL_ROOT', 'https://192.168.99.112:8081');
+    define('URL_ROOT', 'https://192.168.99.113:8081');
     define('SITE_NAME', 'Camagru');
