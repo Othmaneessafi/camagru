f01977c8f3b552f0b83fe35a24fa663d19919170
Othmaneessafi <othmaneessafi1@gmail.com>
2021-01-17 04:48:00 +0100

    code optimized

--- app/Views/pages/index.php
+++ app/Views/pages/index.php
@@ -3,17 +3,16 @@
     require_once CAMAGRU_ROOT . '/Views/inc/nav.php';
 ?>
 
-
-<?php foreach($data['posts'] as $post) : ?>
-        <div class="card card-body mb-3 w-75 h-25 m-auto">
-            <div class="d-flex justify-content-left mb-3 mx-2">
-                <img class="rounded-circle" src="<?php echo $post->profile_img ?>" alt="profile">
-                <h4 class="card-title mx-2 mt-2"><?php echo $post->username; ?></h4>
+    <?php foreach($data['posts'] as $post) : ?>
+        <div class="post-container card card-body mb-3 shadow m-auto">
+            <div class="d-flex justify-content-left h-auto mb-3 mx-2">
+                <img class="post-user  shadow my-auto" src="<?php echo $post->profile_img ?>" alt="profile">
+                <h4 class="card-title mx-2 my-auto h-auto" style="font-size: 1.5rem;"><?php echo $post->username; ?></h4>
             </div>
-            <div class="bg-light p-2 mb-3">
-                <img class="card-img-top" src="<?php echo $post->content; ?>" alt="<?php echo $post->title; ?>">
+            <div class="">
+                <img class="post-img card-img-top" src="<?php echo $post->content; ?>" alt="<?php echo $post->title; ?>">
             </div>
-            <div class="create_date mx-2">
+            <div class="create_date mt-2">
                 <p><?php echo $post->create_at; ?></p>
             </div>
         </div>
